7befce06779221fa3bc44ac1c9b38994c0e77f52
Jimmy Falkbjer <jimmy.falkbjer@gmail.com>
2011-01-16 14:05:56 +0100

    upgraded junit

--- test/src/test/java/com/jfalkbjer/test/AppTest.java
+++ test/src/test/java/com/jfalkbjer/test/AppTest.java
@@ -1,38 +1,22 @@
 package com.jfalkbjer.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test
+	public void testApp()
     {
-        assertTrue( true );
+    	Assert.assertTrue(true);
     }
 }
